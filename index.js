002a1f3977b3646133ae5e79228da446f207856a
farzeenilyaszargar <133712730+farzeenilyaszargar@users.noreply.github.com>
2024-10-14 21:16:00 +0530

    initial upload

---
+++ index.js
@@ -0,0 +1,37 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("boxy");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+
+$(window).scroll(function(){
+  var w = $(window).scrollTop();
+  var f = "translateX(" + w*3.75 + "px) translateY(" + w + "px)";
+
+  $('.skill1').css({
+    transform : f
+  });    
+         
+});
