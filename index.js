f4d21c53660af69d0852cef4600fcda9db2d0c98
farzeenilyaszargar <133712730+farzeenilyaszargar@users.noreply.github.com>
2024-10-23 00:56:49 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,49 @@
+function openPage(pageName, elmnt) {
+  var j, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (j = 0; j < tabcontent.length; j++) {
+    tabcontent[j].style.display = "none";
+  }
+
+  tablinks = document.getElementsByClassName("tablink");
+  for (j = 0; j < tablinks.length; j++) {
+    tablinks[j].style.backgroundColor = "";
+    tablinks[j].style.textShadow="0 0 0";    
+
+  }
+  document.getElementById(pageName).style.display = "flex";
+  elmnt.style.textShadow="0 0 7px white";
+}
+
+
+document.getElementById("open_btn").click();
+
+
+
+
+
+
+function sendMsg()
+{
+  var txtbx = document.getElementById("textar").value;
+  alert("entered is "+txtbx);
+
+}
+
+$('#textar').keyup(function() {
+  var txt_count = $('#counter');
+  var txtct = document.getElementById("textar").value.length;
+  txt_count.text(txtct+"/300");
+
+});
+
+
+
+
+
+
+
+
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -13,25 +59,15 @@ function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("boxy");
   let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+    slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 }
 
-
-$(window).scroll(function(){
-  var w = $(window).scrollTop();
-  var f = "translateX(" + w*3.75 + "px) translateY(" + w + "px)";
-
-  $('.skill1').css({
-    transform : f
-  });    
-         
-});
